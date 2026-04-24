fa42d03a6db16da9c4f9d490617ad23c3d68bf4a
ycy0116-tt <786725257@qq.com>
2026-04-24 12:02:31 +0800

    完成 api.cpp 代码实现

--- api.cpp
+++ api.cpp
@@ -4,70 +4,57 @@
 #include <vector>
 #include <iterator>
 
-//构造函数
+// 初始化类对象
 API::API(){}
 
-//析构函数
+// 释放资源
 API::~API(){}
 
-//输入：表名、Where条件属性名、Where条件值域
-//输出：Table类型对象(包含对应的属性元组)
-//功能：返回包含所有目标属性满足Where条件的记录的表
-//在多条件查询情况下，根据Where下的逻辑条件进行Table的拼接
-//异常：由底层处理
-//如果表不存在，抛出table_not_exist异常
-//如果属性不存在，抛出attribute_not_exist异常
-//如果Where条件中的两个数据类型不匹配，抛出data_type_conflict异常
+// 按条件查询数据表记录，支持多条件逻辑组合筛选
 Table API::selectRecord(std::string table_name, std::vector<std::string> target_attr, std::vector<Where> where, char operation)
 {
+	// 未指定查询字段，直接返回整张数据表
 	if (target_attr.size() == 0) {
 		return record.selectRecord(table_name);
-	} else if (target_attr.size() == 1) {
+	}
+	// 单个查询条件，执行简单条件查询
+	else if (target_attr.size() == 1) {
         return record.selectRecord(table_name, target_attr[0], where[0]);
-    } else {
+    }
+	// 多个查询条件，拆分两次查询后合并结果
+	else {
 		Table table1 = record.selectRecord(table_name, target_attr[0], where[0]);
 		Table table2 = record.selectRecord(table_name, target_attr[1], where[1]);
 
+		// 根据逻辑操作符选择结果合并方式
 		if (operation)
 			return joinTable(table1, table2, target_attr[0], where[0]);
 		else
 			return unionTable(table1, table2, target_attr[0], where[0]);
 	}
 }
-//输入：表名、Where条件属性名、Where条件值域
-//输出：void
-//功能：删除对应条件下的Table内记录(不删除表文件)
-//异常：由底层处理
-//如果表不存在，抛出table_not_exist异常
-//如果属性不存在，抛出attribute_not_exist异常
-//如果Where条件中的两个数据类型不匹配，抛出data_type_conflict异常
+
+// 删除数据表中符合条件的元组数据
 int API::deleteRecord(std::string table_name , std::string target_attr , Where where)
 {
     int result;
+	// 无限定条件，清空整张表数据
 	if (target_attr == "")
 		result = record.deleteRecord(table_name);
+	// 根据字段条件删除指定记录
 	else
 		result = record.deleteRecord(table_name, target_attr, where);
 	return result;
 }
-//输入：表名、一个元组对象
-//输出：void
-//功能：向对应表内插入一条记录
-//异常：由底层处理
-//如果元组类型不匹配，抛出tuple_type_conflict异常
-//如果主键冲突，抛出primary_key_conflict异常
-//如果unique属性冲突，抛出unique_conflict异常
-//如果表不存在，抛出table_not_exist异常
+
+// 向指定数据表中插入一条新的数据元组
 void API::insertRecord(std::string table_name , Tuple& tuple)
 {
 	record.insertRecord(table_name, tuple);
 	return;
 }
-//输入：Table类型对象
-//输出：是否创建成功
-//功能：在数据库中插入一个表的元信息
-//异常：由底层处理
-//如果已经有相同表名的表存在，则抛出table_exist异常
+
+// 新建数据表并保存表结构相关信息
 bool API::createTable(std::string table_name, Attribute attribute, int primary, Index index)
 {
 	record.createTableFile(table_name);
@@ -75,11 +62,8 @@ bool API::createTable(std::string table_name, Attribute attribute, int primary, 
 
 	return true;
 }
-//输入：表名
-//输出：是否删除成功
-//功能：在数据库中删除一个表的元信息，及表内所有记录(删除表文件)
-//异常：由底层处理
-//如果表不存在，抛出table_not_exist异常
+
+// 删除指定数据表，同时清理对应数据文件与目录信息
 bool API::dropTable(std::string table_name)
 {
 	record.dropTableFile(table_name);
@@ -87,13 +71,8 @@ bool API::dropTable(std::string table_name)
 
 	return true;
 }
-//输入：表名，索引名，属性名
-//输出：是否创建成功
-//功能：在数据库中更新对应表的索引信息（在指定属性上建立一个索引）
-//异常：由底层处理
-//如果表不存在，抛出table_not_exist异常
-//如果对应属性不存在，抛出attribute_not_exist异常
-//如果对应属性已经有了索引，抛出index_exist异常
+
+// 为数据表指定字段建立索引结构，提升查询效率
 bool API::createIndex(std::string table_name, std::string index_name, std::string attr_name)
 {
     IndexManager index(table_name);
@@ -101,6 +80,7 @@ bool API::createIndex(std::string table_name, std::string index_name, std::strin
 	std::string file_path = "INDEX_FILE_" + attr_name + "_" + table_name;
 	int type;
 
+	// 更新系统目录内的索引配置
 	catalog.createIndex(table_name, attr_name, index_name);
 	Attribute attr = catalog.getAttribute(table_name);
 	for (int i = 0; i < attr.num; i++) {
@@ -109,18 +89,15 @@ bool API::createIndex(std::string table_name, std::string index_name, std::strin
 			break;
 		}
 	}
+
+	// 创建索引文件并写入对应索引数据
 	index.createIndex(file_path, type);
 	record.createIndex(index , table_name, attr_name);
 
 	return true;
 }
-//输入：表名，索引名
-//输出：是否删除成功
-//功能：删除对应表的对应属性上的索引
-//异常：由底层处理
-//如果表不存在，抛出table_not_exist异常
-//如果对应属性不存在，抛出attribute_not_exist异常
-//如果对应属性没有索引，抛出index_not_exist异常
+
+// 移除数据表中已创建的指定索引
 bool API::dropIndex(std::string table_name, std::string index_name)
 {
     IndexManager index(table_name);
@@ -136,6 +113,8 @@ bool API::dropIndex(std::string table_name, std::string index_name)
 			break;
 		}
 	}
+
+	// 删除索引文件并同步目录信息
 	index.dropIndex(file_path, type);
 	catalog.dropIndex(table_name, index_name);
 	
@@ -144,7 +123,7 @@ bool API::dropIndex(std::string table_name, std::string index_name)
 	return true;
 }
 
-//私有函数，用于多条件查询时的or条件合并
+// 合并两组查询结果，实现多条件或逻辑筛选
 Table API::unionTable(Table &table1, Table &table2, std::string target_attr, Where where)
 {
 	Table result_table(table1);
@@ -153,68 +132,24 @@ Table API::unionTable(Table &table1, Table &table2, std::string target_attr, Whe
 	std::vector<Tuple> tuple2 = table2.getTuple();
     result_tuple = tuple1;
 
-	//std::vector<Tuple>().swap(result_tuple);
-
+	// 遍历属性列表，定位目标条件字段下标
     int i;
     Attribute attr = table1.getAttr();
     for (i = 0; i < 32; i++)
         if (attr.name[i] == target_attr)
             break;
     
+	// 筛选符合或逻辑的数据并入结果集
     for (int j = 0; j < tuple2.size(); j++)
         if (!isSatisfied(tuple2[j], i, where))
             result_tuple.push_back(tuple2[j]);
     
+	// 对最终结果集进行排序整理
     std::sort(result_tuple.begin(), result_tuple.end(), sortcmp);
     return result_table;
-	//std::merge(tuple1.begin(), tuple1.end(), tuple2.begin(), tuple2.end(), std::back_inserter(result_tuple), sortcmp);
-    //result_tuple.erase(unique(result_tuple.begin(), result_tuple.end(), calcmp), result_tuple.end());
-    
-//    int i;
-//    for (i = 0; i < tuple1.size() && i < tuple2.size(); i++) {
-//        std::vector<Data> data1 = tuple1[i].getData();
-//        std::vector<Data> data2 = tuple2[i].getData();
-//        
-//        int j;
-//        for (j = 0; j < data1.size(); j++) {
-//            int flag = 0;
-//
-//            switch (data1[i].type) {
-//                case -1: {
-//                    if (data1[i].datai != data2[i].datai) {
-//                        if (data1[i].datai < data2[i].datai)
-//                            flag = 1;
-//                        else
-//                            flag = -1;
-//                    }
-//                } break;
-//                case 0: {
-//                    if (data1[i].dataf != data2[i].dataf) {
-//                        if (data1[i].dataf < data2[i].dataf)
-//                            flag = 1;
-//                        else
-//                            flag = -1;
-//                    }
-//                } break;
-//                default: {
-//                    if (data1[i].datas < data2[i].datas)
-//                        flag = 1;
-//                    else
-//                        flag = -1;
-//                } break;
-//            }
-//            
-//            if (flag)
-//                break;
-//        }
-//        
-//        switch (flag) {
-//            case 1:
-//        }
-//    }
 }
 
-//私有函数，用于多条件查询时的and条件合并
+// 取两组查询结果交集，实现多条件与逻辑筛选
 Table API::joinTable(Table &table1, Table &table2, std::string target_attr, Where where)
 {
 	Table result_table(table1);
@@ -222,28 +157,23 @@ Table API::joinTable(Table &table1, Table &table2, std::string target_attr, Wher
 	std::vector<Tuple> tuple1 = table1.getTuple();
 	std::vector<Tuple> tuple2 = table2.getTuple();
     
+	// 查找需要判断的目标属性位置
     int i;
     Attribute attr = table1.getAttr();
     for (i = 0; i < 32; i++)
         if (attr.name[i] == target_attr)
             break;
     
+	// 筛选满足共同条件的数据记录
     for (int j = 0; j < tuple2.size(); j++)
         if (isSatisfied(tuple2[j], i, where))
             result_tuple.push_back(tuple2[j]);
     
     std::sort(result_tuple.begin(), result_tuple.end(), sortcmp);
     return result_table;
-//	std::vector<Tuple>().swap(result_tuple);
-//	std::sort(tuple1.begin(), tuple1.end(), sortcmp);
-//	std::sort(tuple2.begin(), tuple2.end(), sortcmp);
-
-    //std::set_intersection(tuple1.begin(), tuple1.end(), tuple2.begin(), tuple2.end(), std::back_inserter(result_tuple), calcmp);
-
-	//return result_table;
 }
 
-//用于对vector的sort时排序
+// 自定义排序规则，根据首字段数据大小排序
 bool sortcmp(const Tuple &tuple1, const Tuple &tuple2)
 {
 	std::vector<Data> data1 = tuple1.getData();
@@ -256,7 +186,7 @@ bool sortcmp(const Tuple &tuple1, const Tuple &tuple2)
     }
 }
 
-//用于对vector对合并时排序
+// 对比两条元组内容，判断数据是否完全一致
 bool calcmp(const Tuple &tuple1, const Tuple &tuple2)
 {
 	int i;
@@ -284,13 +214,13 @@ bool calcmp(const Tuple &tuple1, const Tuple &tuple2)
 			break;
     }
 
-
 	if (i == data1.size())
 		return true;
 	else
 		return false;
 }
 
+// 校验单条元组是否匹配设定的查询条件
 bool isSatisfied(Tuple& tuple, int target_attr, Where where)
 {
     std::vector<Data> data = tuple.getData();
